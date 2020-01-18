34d14d3e0794a382b0a08153c4e95a28a753edf8
Jennifer Ma <jenniferma93@gmail.com>
2020-01-18 13:35:51 -0800

    corrected description of test

--- URLify/URLify.test.js
+++ URLify/URLify.test.js
@@ -6,7 +6,7 @@ describe("#URLify", function () {
     expect(typeof URLify).toEqual("function");
   });
 
-  it("should traverse matrix in a spiral pattern clockwise", function () {
+  it("should replace all spaces within the provided length with '%20'", function () {
     expect(URLify("Mr John Smith    ", 13)).toEqual("Mr%20John%20Smith");
 
     expect(URLify("    ", 4)).toEqual("%20%20%20%20");
